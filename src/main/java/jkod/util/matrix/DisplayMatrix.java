3c6493e4826c18db475580a84cd2ea8dee45c7bc
Jitin Kodian <jitinkodian@gmail.com>
2021-04-07 20:33:04 +0530

    Completed the display matrix

--- src/main/java/jkod/util/matrix/DisplayMatrix.java
+++ src/main/java/jkod/util/matrix/DisplayMatrix.java
@@ -9,19 +9,34 @@ public class DisplayMatrix {
 
 	public static void printArray(int[] array) {
 		printColumns(array.length);
-		printBottomLine(array.length);
+		printSeperator(array.length);
+		printNumber(0);
+		for (int i = 0; i < array.length; i++) {
+			printNumber(array[i]);
+		}
+		System.out.println();
+		printSeperator(array.length);
 	}
 	
 	public static void printArray(int[][] array) {
 		printColumns(array[0].length);
-		printBottomLine(array[0].length);
+		printSeperator(array[0].length);
+		for (int i = 0; i < array.length; i++) {
+			printNumber(i);
+			for (int j = 0; j < array[0].length; j++) {
+				printNumber(array[i][j]);
+			}
+			System.out.println();
+			printSeperator(array.length);
+		}
 	}
 	
 	private static void printColumns(int length) {
-		System.out.print("   |");								//Padding for adding rows
+		System.out.print("    |");									//Padding for adding rows
 		for (int i = 0; i < length; i++) {
 			printNumber(i);
 		}
+		System.out.println();
 	}
 	
 	private static void printNumber(int number) {
@@ -36,12 +51,15 @@ public class DisplayMatrix {
 		}
 	}
 	
-	private static void printBottomLine(int size) {
-		System.out.println();
-		System.out.print("\u203E\u203E\u203E\u203E");			// Overline for the row indexes
-		for (int i = 0; i < size*5; i++) {
-			System.out.print("\u203E");							// Unicode for an overline
+	private static void printSeperator(int size) {
+		System.out.print("\u203E\u203E\u203E\u203E|");				// Overline for the row indexes
+		for (int i = 1; i <= size*5; i++) {
+			if(i % 5 != 0)
+				System.out.print("\u203E");							// Unicode for an overline
+			else
+				System.out.print("|");
 		}
+		System.out.println();
 	}
 	
 }
