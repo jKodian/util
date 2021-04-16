77ed5c5e3f5d743c8b325aa8ce2b8ab8a1d20722
Jitin Kodian <jitinkodian@gmail.com>
2021-04-16 08:03:22 +0530

    Changed the jar name to 0.1

--- src/main/java/jkod/util/matrix/Main.java
+++ src/main/java/jkod/util/matrix/Main.java
@@ -6,14 +6,13 @@ public class Main {
 		
 		  int size = 6; 
 		  int[] testArray1D = RandomMatrix.get1DMatrix(size, 999);
-		  
 		  DisplayMatrix.printArray(testArray1D);
 		
-//		  int rows = 10;
-//		  int columns = 10;
-//		  int[][] testArray2D = RandomMatrix.get2DMatrix(rows, columns, 100);
-//		  
-//		  DisplayMatrix.printArray(testArray2D);
+		  int rows = 10;
+		  int columns = 10;
+		  int[][] testArray2D = RandomMatrix.get2DMatrix(rows, columns, 100);
+		  
+		  DisplayMatrix.printArray(testArray2D);
 		 
 	}
 
